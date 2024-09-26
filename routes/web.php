e0eb44892c1d795deecb161223c399f8f2171a16
timowa <timofejshhetinin@gmail.com>
2024-09-26 19:04:06 +0700

    Правки

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return redirect('/catalog');
 });
 
 Route::group(['middleware' => \App\Http\Middleware\ShopMiddleware::class], function () {
